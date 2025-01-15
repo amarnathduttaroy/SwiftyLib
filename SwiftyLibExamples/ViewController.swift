75013b26ac57241663dfd139f915133480cfc5bb
amarnath.duttaroy <amarnath.dutta@digitalairtech.com>
2025-01-15 18:45:39 +0530

    Initial project setup

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Digital Air Technology and Analytics on 15/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
